f10b790ee31964947db65184241413605dac8b6b
Toki <55911995+tokio-k@users.noreply.github.com>
2021-09-11 17:12:11 +0900

    Add forgetPass

--- src/pages/ForgetPassForm/index.tsx
+++ src/pages/ForgetPassForm/index.tsx
@@ -1,16 +1,23 @@
 import { Button, Card, TextField, Typography } from "@material-ui/core";
+import { useForgetPass } from "../../hooks/Authentication/useForgetPass";
 import useStyles from "./style";
 
 export const ForgetPassForm = () => {
   const styles = useStyles();
+  const { ref, sendEmail, sendSuccess, loading, error } = useForgetPass();
+
   return (
     <Card className={styles.root} variant="outlined">
-      {/* タイトルコンポーネント */}
       <Typography className={styles.margin} component="h1" variant="h5">
         パスワードの再発行
       </Typography>
 
-      {/* メールアドレスフィールド */}
+      {error.has("main") && (
+        <Typography className={styles.margin} color="error">
+          {error.get("main")}
+        </Typography>
+      )}
+
       <label className={`${styles.label} ${styles.margin}`}>
         <Typography>メールアドレス</Typography>
         <TextField
@@ -19,18 +26,31 @@ export const ForgetPassForm = () => {
           size="small"
           fullWidth
           variant="outlined"
+          inputRef={ref.emailRef}
+          error={error.has("email")}
+          helperText={error.has("email") ? error.get("email") : ""}
         />
       </label>
 
-      {/* Submitボタン */}
+      {sendSuccess && (
+        <Typography className={styles.margin} color="primary">
+          ✔︎メールの送信が完了しました。
+        </Typography>
+      )}
+
       <div className={styles.margin}>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={loading}
+          onClick={sendEmail}
+        >
           再発行
         </Button>
       </div>
 
       <div>
-        <Button href="#link" color="primary">
+        <Button href="/login" color="primary">
           ログインはこちら
         </Button>
       </div>
